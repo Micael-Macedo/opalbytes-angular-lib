8d7ff6e545932b3f42bc5d91f5436a5bb92a1e1f
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-12-03 14:38:39 -0300

    feat: add comentario exportacao publica de diretivas

--- projects/ngx-opalbytes-directives/src/public-api.ts
+++ projects/ngx-opalbytes-directives/src/public-api.ts
@@ -1,5 +1,21 @@
 /*
  * Public API Surface of ngx-opalbytes-directives
+ *
+ * Este arquivo é o ponto de entrada da sua biblioteca. Tudo o que for exportado
+ * a partir daqui estará disponível publicamente para as aplicações que consumirem
+ * a 'ngx-opalbytes-directives'.
+ *
+ * Para exportar uma nova diretiva, adicione uma linha de exportação para o arquivo dela.
+ *
+ * Exemplo:
+ * Se você criar uma diretiva chamada 'highlight.directive.ts' dentro da pasta 'src/lib/directives/',
+ * você deve adicioná-la aqui da seguinte forma:
+ *
+ * export * from './lib/directives/highlight.directive';
+ *
+ * Se suas diretivas estiverem agrupadas em um NgModule, você deve exportar o módulo.
+ * Exemplo:
+ *
+ * export * from './lib/directives.module';
+ *
  */
-
-export * from './lib/ngx-opalbytes-directives';
