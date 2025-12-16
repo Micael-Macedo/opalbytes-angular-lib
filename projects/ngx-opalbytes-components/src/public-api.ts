ebae02d4903d332a13acad3ba37e58f5322cb26f
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-12-16 15:17:07 -0300

    feat: liberacao-componentes-base

--- projects/ngx-opalbytes-components/src/public-api.ts
+++ projects/ngx-opalbytes-components/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/ngx-opalbytes-components';
 export * from './lib/shared/components/base-button/base-button'
+export * from './lib/shared/components/base-alert/base-alert'
+export * from './lib/shared/components/base-dialog/base-dialog'
