02ae9ad3b5094623968200639c30e1cb3fbd92f0
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-12-17 15:24:02 -0300

    fix(components): correcao exportacao componente inexistente

--- projects/ngx-opalbytes-components/src/public-api.ts
+++ projects/ngx-opalbytes-components/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-opalbytes-components
  */
 
-export * from './lib/ngx-opalbytes-components';
 export * from './lib/shared/components/base-button/base-button'
 export * from './lib/shared/components/base-alert/base-alert'
 export * from './lib/shared/components/base-dialog/base-dialog'
