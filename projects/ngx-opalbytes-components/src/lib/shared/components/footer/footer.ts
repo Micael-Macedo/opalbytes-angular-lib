7947486d2e33d3a1c0f8b87992312ce5be74f439
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2025-12-22 16:52:39 -0300

    feat(components): add adicao elementos em rodape

--- projects/ngx-opalbytes-components/src/lib/shared/components/footer/footer.ts
+++ projects/ngx-opalbytes-components/src/lib/shared/components/footer/footer.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
 
 @Component({
-  standalone: true,
   selector: "cao-footer",
   templateUrl: "./footer.html",
   styleUrl: "./footer.css",
+  imports: [ CommonModule ]
 })
 export class Footer {
   currentYear: number = new Date().getFullYear();
-  @Input() currentRoute = "";
-  @Input() footerText = "";
+  @Input() currentRoute?:string;
+  @Input() footerText?:string;
+  @Input() isFixed = true;
 }
