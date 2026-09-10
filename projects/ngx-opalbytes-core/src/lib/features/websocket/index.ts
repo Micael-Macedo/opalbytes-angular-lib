4a61fc4d4b6d9652654398be6bd6782926e662a6
Micael-Macedo <micaelm2009@hotmail.com>
2026-09-10 15:38:54 -0300

    feat: add unused content

--- projects/ngx-opalbytes-core/src/lib/features/websocket/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Barrel export para services WebSocket
- * Facilita importação centralizada
- */
-
-export * from './websocket-connection';
-export * from './base-websocket.service';
-export * from './websocket-manager.service';
